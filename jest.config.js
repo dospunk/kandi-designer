c09842847e5da2b5b3b610dfd0ecaf50f1849335
Connor (Windows) <dospunk@users.noreply>
2020-10-18 14:06:54 -0400

    unit tests now work!

--- jest.config.js
+++ jest.config.js
@@ -79,7 +79,11 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+	"^\./kandi\.js$": "\./kandi",
+	"^\./cursor\.js$": "\./cursor",
+	"^\./dom\.js$": "\./dom"
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
