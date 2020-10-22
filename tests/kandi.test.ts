a29616fef7cd5d069369115f7a5a109df5f99da7
Connor (Windows) <dospunk@users.noreply>
2020-10-22 19:57:39 -0400

    Adds a test for Kandi.isEmpty

--- tests/kandi.test.ts
+++ tests/kandi.test.ts
@@ -9,12 +9,20 @@ test('create a Kandi object', ()=>{
 	expect(() => new Kandi(ctx, canv.width, canv.height)).not.toThrowError();
 });
 
+test("kandi is empty when design is all 0's", ()=>{
+	const k = new Kandi(ctx, canv.width, canv.height);
+	expect(k.isEmpty()).toBe(true);
+	k.design[0][0] = 1;
+	expect(k.isEmpty()).toBe(false);
+	k.design[0][0] = 0;
+	expect(k.isEmpty()).toBe(true);
+});
+
 //todo: test Kandi.draw
 //todo: test Kandi.paint
 //todo: test Kandi.getHeight
 //todo: test Kandi.getWidth
 //todo: test Kandi.setHeight
 //todo: test Kandi.setWidth
-//todo: test Kandi.isEmpty
 //todo: test Kandi.shiftLeft
 //todo: test Kandi.shiftRight
